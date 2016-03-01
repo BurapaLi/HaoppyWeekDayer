e7f5b3d4d4b0a40a1c58bf6724c6857df33ee9ad
李志鹏 <1713396133@qq.co>
2016-03-01 10:26:45 +0800

    bug修复

--- HappyWeekDayer/Library/Header.h
+++ HappyWeekDayer/Library/Header.h
@@ -27,11 +27,7 @@ typedef NS_ENUM(NSInteger,ClassifyType){
 };
 //首页数据接口
 #define kIndexDataList @"http://e.kumi.cn/app/v1.3/index.php?_s_=02a411494fa910f5177d82a6b0a63788&_t_=1451307342&channelid=appstore&cityid=1&lat=34.62172291944134&limit=30&lng=112.4149512442411&page=1"
-http://e.kumi.cn/app/v1.3/index.php?_s_=02a411494fa910f5177d82a6b0a63788&_t_=1451307342
-&channelid=appstore
-&cityid=1
-&lat=34.62172291944134&limit=30
-&lng=112.4149512442411&page=1
+
 
 //url http://e.kumi.cn/app/v1.3/index.php
 
